68522d1a30ea198976ade1d1366ab70f4529e253
Ariel Klevecz <arielklevecz@Ariels-MacBook-Pro.local>
2024-04-04 12:22:49 -0700

    headers live

--- src/pages/img/[name].ts
+++ src/pages/img/[name].ts
@@ -3,6 +3,18 @@ import type { APIRoute } from 'astro';
 export const GET: APIRoute = async ({ params, request, locals }) => {
     const {name} = params
     const img = await locals.runtime.env.R2.get(name)
+    if (!img) {
+        return new Response('Not found', {status: 404})
+    
+    }
     const readableStream = img.body
-    return new Response(readableStream)
+
+    // Create a new headers object
+    const headers = new Headers();
+
+    // Write the image metadata to the headers
+    img.writeHttpMetadata(headers);
+    headers.set('etag', img.httpEtag);
+    // console.log(headers)
+    return new Response(readableStream, {headers});
 }
