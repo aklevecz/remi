3c6c7afae15a9a04d2b35e81f6307aa806941a60
Ariel Klevecz <arielklevecz@Ariels-MacBook-Pro.local>
2024-04-05 00:06:53 -0700

    astro locals

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -1,6 +1,3 @@
-// const SENDGRID_API_KEY = import.meta.env.SENDGRID_API_KEY;
-// const EMAIL_SENDER_ADDRESS = import.meta.env.EMAIL_SENDER_ADDRESS;
-
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 const EMAIL_SENDER_ADDRESS = process.env.EMAIL_SENDER_ADDRESS;
 type Props = {
@@ -8,21 +5,27 @@ type Props = {
   subject: string;
   message: string;
 };
-export const sendEmail = async ({ email, subject, message }: Props) => {
-  const data = {
-    personalizations: [{ to: [{ email }] }],
-    from: { email: EMAIL_SENDER_ADDRESS },
-    subject,
-    content: [{ type: "text/html", value: message }],
+
+const email = ({ apiKey, from }: { apiKey: string; from: string }) => {
+  const sendEmail = async ({ email, subject, message }: Props) => {
+    const data = {
+      personalizations: [{ to: [{ email }] }],
+      from: { email: from },
+      subject,
+      content: [{ type: "text/html", value: message }],
+    };
+    return fetch("https://api.sendgrid.com/v3/mail/send", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.text())
+      .catch((error) => console.error("Error:", error));
   };
-  return fetch("https://api.sendgrid.com/v3/mail/send", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${SENDGRID_API_KEY}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => response.text())
-    .catch((error) => console.error("Error:", error));
+  return { sendEmail };
 };
+
+export default email;
