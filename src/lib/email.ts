baa0239b02bb52500840457873ae82738b5f1d7b
Ariel Klevecz <arielklevecz@Ariels-MacBook-Pro.local>
2024-04-04 19:32:28 -0700

    test sticky

---
+++ src/lib/email.ts
@@ -0,0 +1,25 @@
+const SENDGRID_API_KEY = import.meta.env.SENDGRID_API_KEY;
+const EMAIL_SENDER_ADDRESS = import.meta.env.EMAIL_SENDER_ADDRESS;
+type Props = {
+  email: string;
+  subject: string;
+  message: string;
+};
+export const sendEmail = async ({ email, subject, message }: Props) => {
+  const data = {
+    personalizations: [{ to: [{ email }] }],
+    from: { email: EMAIL_SENDER_ADDRESS },
+    subject,
+    content: [{ type: "text/html", value: message }],
+  };
+  return await fetch("https://api.sendgrid.com/v3/mail/send", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${SENDGRID_API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.text())
+    .catch((error) => console.error("Error:", error));
+};
