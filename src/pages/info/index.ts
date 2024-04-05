5dfb73a43851290429ec1badcf6bbea1aad0aeb7
Ariel Klevecz <arielklevecz@Ariels-MBP.charter.com>
2024-04-05 13:42:07 -0700

    add info endpint

---
+++ src/pages/info/index.ts
@@ -0,0 +1,7 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ params, request, locals }) => {
+    // obfuscated with 9s
+  return new Response("71948909897117");
+};
+
