fbdce467b385cb5455ea745e0458675acaedf9a5
Ariel Klevecz <arielklevecz@Ariels-MacBook-Pro.local>
2024-04-15 08:59:56 -0700

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import db from "@astrojs/db";
 export default defineConfig({
   output: "server",
   adapter: cloudflare({
+    imageService: "cloudflare",
     platformProxy: {
       enabled: true,
     },
