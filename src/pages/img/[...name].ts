265b017c08e649f621d33e797c2cca98042aaeb6
Ariel Klevecz <arielklevecz@Ariels-MacBook-Pro.local>
2024-04-15 11:15:09 -0700

    test

--- src/pages/img/[...name].ts
+++ src/pages/img/[...name].ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ params, request, locals }) => {
-  const { name } = params;
+  let { name } = params;
   const img = await locals.runtime.env.R2.get(name);
   if (!img) {
     return new Response("Not found", { status: 404 });
